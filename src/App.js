0d90c58c83080b73fb7b51dcf4514afedc4a044c
Mihir478 <mihir.h.sanghavi@gmail.com>
2021-03-10 16:36:52 -0500

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik, Field, FormikProvider } from 'formik';
-import * as yup from 'yup';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
+import { 
+  Box,
+  Button, 
+  FormControl, 
+  FormLabel,
+  Grid, 
+  InputLabel,  
+  MenuItem, 
+  Select, 
+  TextField, 
+  Typography,
+  Paper,
+} from '@material-ui/core';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+import { Graph } from 'react-d3-graph';
+import * as yup from 'yup';
 import './App.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -50,21 +55,123 @@ const validationSchema = yup.object({
     .required('Fund type is required')
 });
 
-const AddFund = () => {
+const config = {
+  height: 400,
+  width: 800,
+  node: {
+    color: '#2196f3',
+    fontColor: 'black',
+    fontSize: 12,
+    fontWeight: 'normal',
+    highlightColor: 'SAME',
+    highlightFontSize: 12,
+    highlightFontWeight: 'normal',
+    highlightStrokeColor: 'SAME',
+    highlightStrokeWidth: 'SAME',
+    labelProperty: 'name',
+    renderLabel: true,
+    size: 1000,
+    strokeColor: 'none',
+    strokeWidth: 1.5,
+    symbolType: 'circle'
+  },
+  link: {
+    color: 'gray',
+    fontColor: 'gray',
+    fontSize: 8,
+    highlightColor: 'SAME',
+    labelProperty: 'label',
+    mouseCursor: 'pointer',
+    opacity: 1,
+    fontWeight: 'normal',
+    highlightFontSize: 8,
+    highlightFontWeight: 'normal',
+    highlightStrokeColor: 'SAME',
+    highlightStrokeWidth: 'SAME',
+    renderLabel: true,
+    semanticStrokeWidth: false,
+    strokeWidth: 1,
+    markerHeight: 6,
+    markerWidth: 6,
+    strokeDasharray: 0,
+    strokeDashoffset: 0
+  }
+};
+
+// globals within app
+const data = {
+  nodes: [],
+  links: []
+};
+
+const fundAttributes = ['manager', 'year', 'type', 'status'];
+
+const linkCache = {
+  manager: [],
+  year: [],
+  type: [],
+  status: []
+};
+
+// utility functions
+const compareAndUpdateLinkCache = (a, b, prop, links) => {
+  if (a[prop] === b[prop])
+    links.push({ 
+      source: a.id,
+      target: b.id,
+      label: prop === 'status' ? (a[prop] ? 'Open' : 'Closed') : a[prop].toString()
+    });
+}
+
+let actionsRef = null;
+
+const onSubmitHandler = (values, actions) => {
+  // cache actions to call from graph
+  if(!actionsRef) {
+    actionsRef = actions;
+  }
+  // add fund to graph
+  data.nodes.push({
+    id: Math.random(),
+    // place randomly offsetted from the center 
+    x: (config.width / 2) + Math.random() * 100 * (Math.random() < 0.5 ? -1 : 1),
+    y: (config.height / 2) + Math.random() * 100 * (Math.random() < 0.5 ? -1: 1),
+    ...values
+  });
+  // update link cache complexity O(an) where n is # of nodes and a is # of fund attributes
+  for (var i = 0; i < data.nodes.length - 1; i++)
+    for (const prop of fundAttributes)
+    compareAndUpdateLinkCache(
+      data.nodes[i],
+      data.nodes[data.nodes.length - 1], // node that was just added
+      prop,
+      linkCache[prop]
+    )
+  // reset to add another fund easily
+  actions.resetForm();
+}
+
+const connectBy = (e, connectByProp) => {
+  data.links = linkCache[connectByProp];
+  if(actionsRef)
+    actionsRef.resetForm();
+}
+
+const UI = () => {
   const classes = useStyles();
-  
+  const [connectedBy, setConnectedBy] = useState('');
+
+  // hook cannot be called at the top level
   const formik = useFormik({
     initialValues: {
       name: '',
       manager: '',
-      year: null,
+      year: '',
       type: '',
-      open: true
+      status: true // open
     },
     validationSchema: validationSchema,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
+    onSubmit: onSubmitHandler
   });
 
   return (
@@ -75,9 +182,9 @@ const AddFund = () => {
             <form onSubmit={formik.handleSubmit}>
               <Grid item xs={12}>
                 <TextField
-                  id="name"
-                  name="name"
-                  label="Name"
+                  id='name'
+                  name='name'
+                  label='Fund Name'
                   value={formik.values.name}
                   onChange={formik.handleChange}
                   error={formik.touched.name && Boolean(formik.errors.name)}
@@ -87,10 +194,10 @@ const AddFund = () => {
               <Grid item xs={12}>
                 <Box mt={1}>
                   <TextField
-                    id="manager"
-                    name="manager"
-                    label="Fund Manager"
-                    type="manager"
+                    id='manager'
+                    name='manager'
+                    label='Fund Manager'
+                    type='manager'
                     value={formik.values.manager}
                     onChange={formik.handleChange}
                     error={formik.touched.manager && Boolean(formik.errors.manager)}
@@ -101,10 +208,10 @@ const AddFund = () => {
               <Grid item xs={12}>
                 <Box mt={1}>
                   <TextField
-                    id="year"
-                    name="year"
-                    label="Launch Year"
-                    type="year"
+                    id='year'
+                    name='year'
+                    label='Launch Year'
+                    type='year'
                     value={formik.values.year}
                     onChange={formik.handleChange}
                     error={formik.touched.year && Boolean(formik.errors.year)}
@@ -115,16 +222,14 @@ const AddFund = () => {
               <Grid item xs={12}>
                 <Box mt={1}>
                   <FormControl className={classes.formControl}>
-                    <InputLabel id="type-select-label">Type</InputLabel>
+                    <InputLabel id='type-select-label'>Fund Type</InputLabel>
                     <Select
-                      labelId="type-select-label"
-                      id="type-select"
-                      name="type"
-                      type="type"
+                      labelId='type-select-label'
+                      id='type-select'
+                      name='type'
+                      type='type'
                       value={formik.values.type}
                       onChange={formik.handleChange}
-                      error={formik.touched.type && Boolean(formik.errors.type)}
-                      helperText={formik.touched.type && formik.errors.type}
                     >
                       <MenuItem value={'Venture Capital'}>Venture Capital</MenuItem>
                       <MenuItem value={'Real Estate'}>Real Estate</MenuItem>
@@ -136,11 +241,11 @@ const AddFund = () => {
               <Grid item xs={12}>
                 <Box mt={1}>
                 <FormControl className={classes.formControl}>
-                  <FormLabel>Status</FormLabel>
+                  <FormLabel>Fund Status</FormLabel>
                   <FormikProvider value={formik}>
                     <label>
-                      <Field type="checkbox" name="open" />
-                      {`${formik.values.open ? 'Open' : 'Closed'}`}
+                      <Field type='checkbox' name='status' />
+                      {`${formik.values.status ? 'Open' : 'Closed'}`}
                     </label>
                   </FormikProvider>
                 </FormControl>
@@ -148,12 +253,55 @@ const AddFund = () => {
               </Grid>
               <Grid item xs={12}>
                 <Box mt={1}>
-                  <Button color="primary" variant="contained" type="submit">
+                  <Button
+                    color='primary'
+                    variant='contained'
+                    type='submit'
+                    onSubmit={formik.handleChange}>
                     Add Fund
                   </Button>
                 </Box>
               </Grid>
             </form>
+          </Paper>
+        </Box>
+        <Box mt={4}>
+          <Paper className={classes.paper}>
+            <Grid item xs={12}>
+              <Typography>Connect By:</Typography>
+              <ToggleButtonGroup
+                onChange={connectBy}
+                aria-label="connect by"
+                exclusive
+                value={connectedBy}
+              >
+                <ToggleButton value="manager" aria-label="manager" 
+                  onClick={() => setConnectedBy('manager')}>
+                  Fund Manager
+                </ToggleButton>
+                <ToggleButton value="year" aria-label="year"
+                  onClick={() => setConnectedBy('year')}>
+                  Launch Year
+                </ToggleButton>
+                <ToggleButton value="type" aria-label="type"
+                 onClick={() => setConnectedBy('type')}>
+                  Fund Type
+                </ToggleButton>
+                <ToggleButton value="status" aria-label="status"
+                  onClick={() => setConnectedBy('status')}>
+                  Fund Status
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
+            <Grid item xs={12}>
+              <Box mt={2} border={1}>
+                <Graph
+                  id="fund-similarity-graph"
+                  data={data}
+                  config={config}
+                />
+              </Box>
+            </Grid>
           </Paper>
         </Box>
       </Grid>    
@@ -163,9 +311,7 @@ const AddFund = () => {
 
 function App() {
   return (
-    <div className="App">
-      <AddFund />
-    </div>
+    <UI />
   );
 }
 
